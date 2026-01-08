18b8f13a86977d1c2c7862672244897e3e16f908
pranjalghodechor2005-ux <pranjalghodechor2005@gmail.com>
2026-01-08 20:42:26 +0530

    Update MySQL connection details

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var util = require("util");
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"a2z47_ecom"
+    host:"b9u6sr8ecrxvaymrhz4f-mysql.services.clever-cloud.com",
+    user:"uthgbycklqwuupy0",
+    password:"rwD6wVw8uemCvzhXREpE",
+    database:"b9u6sr8ecrxvaymrhz4f"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
