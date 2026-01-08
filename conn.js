8ceed7992942b53440e832754d9454659355cfac
pranjalghodechor2005-ux <pranjalghodechor2005@gmail.com>
2026-01-08 20:46:52 +0530

    Fix quotes in connection configuration

--- conn.js
+++ conn.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var util = require("util");
 var conn = mysql.createConnection({
-    host:"b9u6sr8ecrxvaymrhz4f-mysql.services.clever-cloud.com",
-    user:"uthgbycklqwuupy0",
+    host:'b9u6sr8ecrxvaymrhz4f-mysql.services.clever-cloud.com',
+    user:'uthgbycklqwuupy0',
     password:"rwD6wVw8uemCvzhXREpE",
     database:"b9u6sr8ecrxvaymrhz4f"
 });
