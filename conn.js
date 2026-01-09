bc7c339b3f0276fb2b84a225a56167de44483db0
pranjalghodechor2005-ux <pranjalghodechor2005@gmail.com>
2026-01-09 10:35:02 +0530

    Change MySQL connection settings
    Updated MySQL connection details to new credentials and host.

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var util = require("util");
 var conn = mysql.createConnection({
-    host:'bc5wo0gfnqqznrvuvag7-mysql.services.clever-cloud.com',
-    user:'urhqxqyaxhsanrr0',
-    password:"nM7LRALOL9JeHCufWrux",
-    database:"bc5wo0gfnqqznrvuvag7"
+    host:'bu48mgvbcgai5s68mva4-mysql.services.clever-cloud.com',
+    user:'ujerstgnezroejrz',
+    password:"9qUXLMfSjw67KoVxEtCQ",
+    database:"bu48mgvbcgai5s68mva4"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
